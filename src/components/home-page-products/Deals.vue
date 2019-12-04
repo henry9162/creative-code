69f8da47393238bf538eee99f5fbc89b4cd02821
Henry Ekwonwa <ohekwonwa@sidmach.com>
2019-12-04 16:38:47 +0100

    refactorying -- WIP

--- src/components/home-page-products/Deals.vue
+++ src/components/home-page-products/Deals.vue
@@ -3,7 +3,7 @@
     <v-container fluid>
         <v-row class="mx-4">
             <v-col md="12">
-                <v-card>
+                <v-card flat>
                     <v-row class="px-3">   
                         <div class="d-flex red white--text px-4 pt-5 justify-space-between" style="width: 100%">
                             <div class="title">
@@ -23,79 +23,8 @@
                         </div>
                     </v-row>
 
-                    <v-row class="px-5 pt-2">
-                        <v-col v-for="product in deals" :key="product.id" md="3">
-                            <v-hover v-slot:default="{ hover }">
-                                <v-card color="white" max-width="400" :elevation="hover ? 20 : 0">
-                                    <v-img :aspect-ratio="16/12" :src="product.image_front">
-                                        <v-expand-transition>
-                                            <div v-if="hover"
-                                                class="d-flex transition-fast-in-fast-out darken-2 font-weight-bold v-card--reveal display-3 white--text"
-                                                :class="product.color.name"
-                                                style="height: 100%;"
-                                            >
-                                                <v-btn @click="$modal.show('product-modal', { product: product })" fab>
-                                                    <v-icon large>mdi-magnify-plus-cursor</v-icon>
-                                                </v-btn>
-                                        </div>
-                                        </v-expand-transition>
-
-                                        <div class="product-label-div">
-                                            <div class="product-labels-left">
-                                                <div class="product-labels-left-btn mt-10">
-                                                    <v-btn class="ml-0" min-width="180" tile rounded small :color="product.color.name + ' darken-3'" dark>Custom Labels</v-btn>
-                                                </div>
-                                            </div>
-                                            <div class="product-labels-right">
-                                                <div class="my-1">
-                                                    <v-btn rounded x-small color="orange" dark>
-                                                        Premium <v-icon small right>mdi-star</v-icon>
-                                                    </v-btn>
-                                                </div>
-                                                <div class="text-right mr-1 my-1"><v-btn fab x-small color="primary" dark>New</v-btn></div>
-                                                <div class="text-right mr-1 my-1"><v-btn fab x-small class="black--text" color="yellow" dark>Hot</v-btn></div>
-                                                <div class="text-right mr-1 my-1"><v-btn fab x-small color="danger" dark>Hot</v-btn></div>
-                                            </div>
-                                        </div>
-                                    </v-img>
-
-                                    <v-card-text class="pt-6" style="position: relative;">
-                                        <v-hover v-slot:default="{ hover }">
-                                            <v-btn @click="addToCart({ productId: product.id, quantity: quantity, inventory: product.inventory })" :elevation="hover ? 12 : 2" absolute :color="product.color.name" class="white--text" fab small right top>
-                                                <v-icon>mdi-cart</v-icon>
-                                            </v-btn>
-                                        </v-hover>
-                                        <div class="font-weight-light grey--text subtitle-2 mb-1">For the perfect meal</div>
-                                        <div style="display: flex; justify-content: space-between; max-height: 20px">
-                                            <h3 class="subtitle-1 font-weight-bold grey--text text--darken-3" v-text="product.title"></h3>
-                                            <h3 class="font-weight-black title" v-text="'$' + product.price"></h3>
-                                        </div>
-                                    </v-card-text>
-
-                                    <v-card-actions>
-                                        <v-btn x-small text>
-                                            <v-icon small color="green darken-1" class="mr-1">mdi-currency-ngn</v-icon> Buy Now
-                                        </v-btn>
-                                        <v-btn x-small text color="purple">
-                                            <v-icon left>mdi-email-outline</v-icon> Ask Question
-                                        </v-btn>
-
-                                        <div class="flex-grow-1"></div>
-
-                                        <v-btn icon @click="toggleProductDropdown(product.id)">
-                                            <v-icon>{{ show == true && productId == product.id ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-                                        </v-btn>
-                                    </v-card-actions>
-
-                                    <v-expand-transition>
-                                        <div v-show="show == true && productId == product.id">
-                                            <v-card-text v-text="product.description"></v-card-text>
-                                        </div>
-                                    </v-expand-transition>
-                                </v-card>
-                            </v-hover>
-                        </v-col>
-                    </v-row>
+                    <product-list :visible="true" :gridValue="4" :products="deals"></product-list>
+                    
                 </v-card>
             </v-col>
         </v-row>
@@ -105,13 +34,18 @@
 <script>
 import { mapActions, mapGetters } from 'vuex'
 import cart from '../../mixins/Carts.js'
+import productList from '../ProductList'
+
 export default {
+    components: { productList },
+
+    mixins: [cart],
+
+
     data: () => ({
         show: false,
         productId: '',
     }),
-
-    mixins: [cart],
 
     computed: {
         deals() { return this.$store.getters.products.slice(0, 4) }
--- src/components/home-page-products/HomeProductFilters.vue
+++ src/components/home-page-products/HomeProductFilters.vue
@@ -35,86 +35,11 @@
         <!-- Product Cards -->
         <product-modal @cartAdded="activateSnackbar"></product-modal>
 
-        <v-container class="pt-0" fluid>
-            <v-row class="mx-4">
-                <v-col v-for="product in products" :key="product.id" md="3">
-                    <v-hover v-slot:default="{ hover }">
-                        <v-card color="white" max-width="400" :elevation="hover ? 20 : 1">
-                            <v-img :aspect-ratio="16/12" :src="product.image_front">
-                                <v-expand-transition>
-                                    <div v-if="hover"
-                                        class="d-flex transition-fast-in-fast-out darken-2 font-weight-bold v-card--reveal display-3 white--text"
-                                        :class="product.color"
-                                        style="height: 100%;"
-                                    >
-                                        <v-btn @click="$modal.show('product-modal', { product: product })" fab>
-                                            <v-icon large>mdi-magnify-plus-cursor</v-icon>
-                                        </v-btn>
-                                    
-                                </div>
-                                </v-expand-transition>
-
-                                <div class="product-label-div">
-                                    <div class="product-labels-left">
-                                        <div class="product-labels-left-btn mt-10">
-                                            <v-btn class="ml-0" min-width="180" tile rounded small :color="product.color + ' darken-3'" dark>Custom Labels</v-btn>
-                                        </div>
-                                    </div>
-                                    <div class="product-labels-right">
-                                        <div class="my-1">
-                                            <v-btn rounded x-small color="orange" dark>
-                                                Premium <v-icon small right>mdi-star</v-icon>
-                                            </v-btn>
-                                        </div>
-                                        <div class="text-right mr-1 my-1"><v-btn fab x-small color="primary" dark>New</v-btn></div>
-                                        <div class="text-right mr-1 my-1"><v-btn fab x-small class="black--text" color="yellow" dark>Hot</v-btn></div>
-                                        <div class="text-right mr-1 my-1"><v-btn fab x-small color="danger" dark>Hot</v-btn></div>
-                                    </div>
-                                </div>
-                            </v-img>
-
-                            <v-card-text class="pt-6" style="position: relative;">
-                                <v-hover v-slot:default="{ hover }">
-                                    <v-btn :elevation="hover ? 12 : 2" absolute :color="product.color.name" class="white--text" fab small right top
-                                        @click="addToCart({ productId: product.id, quantity: quantity, inventory: product.inventory })">
-                                        <v-icon>mdi-cart</v-icon>
-                                    </v-btn>
-                                </v-hover>
-                                <div class="font-weight-light grey--text subtitle-2 mb-1">For the perfect meal</div>
-                                <div style="display: flex; justify-content: space-between; max-height: 20px">
-                                    <h3 class="subtitle-1 font-weight-bold grey--text text--darken-3" v-text="product.title"></h3>
-                                    <h3 class="font-weight-black title" v-text="'$' + product.price"></h3>
-                                </div>
-                            </v-card-text>
-
-                            <v-card-actions>
-                                <v-btn x-small text>
-                                    <v-icon small color="green darken-1" class="mr-1">mdi-currency-ngn</v-icon> Buy Now
-                                </v-btn>
-                                <v-btn x-small text color="purple">
-                                    <v-icon left>mdi-email-outline</v-icon> Ask Question
-                                </v-btn>
-
-                                <div class="flex-grow-1"></div>
-
-                                <v-btn icon @click="toggleProductDropdown(product.id)">
-                                    <v-icon>{{ show == true && productId == product.id ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-                                </v-btn>
-                            </v-card-actions>
-
-                            <v-expand-transition>
-                                <div v-show="show == true && productId == product.id">
-                                    <v-card-text>
-                                        {{ product.description }}
-                                    </v-card-text>
-                                </div>
-                            </v-expand-transition>
-                        </v-card>
-                    </v-hover>
-                </v-col>
-            </v-row>
-        </v-container>
-
+        <div class="mx-8">
+            <product-list :visible="true" :gridValue="4" :products="products"></product-list>
+        </div>  
+        
+        <!-- Deals -->
         <deals />
 
     </div>
@@ -123,10 +48,11 @@
 <script>
 import productModal from './ProductModal'
 import deals from './Deals'
+import productList from '../ProductList'
 import { mapState, mapGetters, mapActions } from 'vuex'
 
 export default {
-    components: { productModal, deals },
+    components: { productModal, deals, productList },
 
     data: () => ({
         active: 'Latest',
@@ -159,9 +85,9 @@ export default {
         selectFilter(title){
             if (title == 'Latest') this.products = this.allProducts.sort((a, b) => (a.date > b.date) ? -1 : 1);
             if (title == 'Featured') this.products = this.allProducts.filter(product => product.featured == true);
-            if (title == 'Men') this.products = this.allProducts.filter(product => product.category == 'men');
-            if (title == 'Women') this.products = this.allProducts.filter(product => product.category == 'women');
-            if (title == 'Kids') this.products = this.allProducts.filter(product => product.category == 'kids');
+            if (title == 'Men') this.products = this.allProducts.filter(product => product.category.name == 'Men');
+            if (title == 'Women') this.products = this.allProducts.filter(product => product.category.name == 'Women');
+            if (title == 'Kids') this.products = this.allProducts.filter(product => product.category.name == 'Kids');
         },
     },
 
